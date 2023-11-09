a8299e1ea1ee9d99f02e5996691cf822e57a447e
ocLucyk <150388749+ocLucyk@users.noreply.github.com>
2023-11-10 09:59:57 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,77 @@
 input.onButtonPressed(Button.AB, function () {
-    led.stopAnimation()
-    for (let index = 0; index < 4; index++) {
-        basic.pause(200)
-        if (Math.randomBoolean()) {
+    if (normal_1) {
+        led.stopAnimation()
+        for (let index = 0; index < 3; index++) {
+            music.play(music.createSoundExpression(WaveShape.Sine, 880, 620, 255, 255, 5000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.InBackground)
             feeding_1.showImage(0)
-        } else {
             feeding_2.showImage(0)
+            Happy.showImage(0)
         }
+        health += 1
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 220), music.PlaybackMode.UntilDone)
     }
-    Happy.showImage(0)
+    if (normal_2) {
+        led.stopAnimation()
+        for (let index = 0; index < 4; index++) {
+            music.play(music.createSoundExpression(WaveShape.Sine, 880, 620, 255, 255, 5000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.InBackground)
+            feeding_1.showImage(0)
+            feeding_2.showImage(0)
+            Happy.showImage(0)
+        }
+        health += 1
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 220), music.PlaybackMode.UntilDone)
+    }
+    if (Teen) {
+        led.stopAnimation()
+        for (let index = 0; index < 4; index++) {
+            music.play(music.createSoundExpression(WaveShape.Sine, 880, 620, 255, 255, 5000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.InBackground)
+            tfeed1.showImage(0)
+            tfeed2.showImage(0)
+            teen_happy.showImage(0)
+        }
+        health += 1
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 220), music.PlaybackMode.UntilDone)
+    }
+    if (teen2) {
+        led.stopAnimation()
+        for (let index = 0; index < 4; index++) {
+            music.play(music.createSoundExpression(WaveShape.Sine, 880, 620, 255, 255, 5000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.InBackground)
+            tfeed1.showImage(0)
+            tfeed2.showImage(0)
+            teen_happy.showImage(0)
+        }
+        health += 1
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 220), music.PlaybackMode.UntilDone)
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.play(music.stringPlayable("A E D F E G B C5 ", 151), music.PlaybackMode.InBackground)
     basic.showString("HEALTH IS" + health)
 })
+let tfeed2: Image = null
+let tfeed1: Image = null
+let teen_happy: Image = null
+let teen2: Image = null
+let Teen: Image = null
 let feeding_2: Image = null
 let feeding_1: Image = null
 let Happy: Image = null
+let normal_2: Image = null
+let normal_1: Image = null
 let health = 0
 health = 1
-let normal_1 = images.createImage(`
+normal_1 = images.createImage(`
     . . . . .
     . # . # .
     . . # . .
     . # # # .
     . . . . .
     `)
-let normal_2 = images.createImage(`
+normal_2 = images.createImage(`
     . . . . .
     . . . . .
     . # . # .
@@ -53,25 +99,39 @@ feeding_2 = images.createImage(`
     . # # # .
     . . . . .
     `)
-let Teen = images.createImage(`
+Teen = images.createImage(`
     . # . # .
     . . . . .
     . . # . .
     . # # # .
     . . . . .
     `)
-let teen2 = images.createImage(`
+teen2 = images.createImage(`
     . . . . .
     . # . # .
     . . . . .
     . . # . .
     . # # # .
     `)
-let teen_happy = images.createImage(`
+teen_happy = images.createImage(`
     . . . . .
     . # . # .
     . . . . .
     # . # . #
+    . # # # .
+    `)
+tfeed1 = images.createImage(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . # # # .
+    `)
+tfeed2 = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . . # . .
     . # # # .
     `)
 let adult = images.createImage(`
@@ -95,19 +155,77 @@ let adulthappy = images.createImage(`
     # . # . #
     . # # # .
     `)
+let af1 = images.createImage(`
+    # # . # #
+    # # . # #
+    . . # . .
+    . # . # .
+    . # # # .
+    `)
+let af2 = images.createImage(`
+    . . . . .
+    # # . # #
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
 if (health > 3) {
     normal_1 = Teen
+    normal_2 = teen2
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
+}
+if (health > 6) {
+    Teen = adult
+    teen2 = adult2
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
 }
 loops.everyInterval(1000, function () {
     if (input.buttonIsPressed(Button.AB)) {
         led.stopAnimation()
     }
-    if (Math.randomBoolean()) {
-        normal_1.showImage(randint(-1, 1))
-    } else {
-        normal_2.showImage(randint(-1, 1))
+    if (health < 3) {
+        if (Math.randomBoolean()) {
+            normal_1.showImage(0)
+        } else {
+            normal_2.showImage(0)
+        }
+    }
+    if (health < 3) {
+        if (Math.randomBoolean()) {
+            normal_1.showImage(0)
+        } else {
+            normal_2.showImage(0)
+        }
+    }
+    if (health > 3) {
+        if (Math.randomBoolean()) {
+            Teen.showImage(0)
+        } else {
+            teen2.showImage(0)
+        }
+    }
+    if (health > 3) {
+        if (Math.randomBoolean()) {
+            Teen.showImage(0)
+        } else {
+            teen2.showImage(0)
+        }
+    }
+    if (health > 6) {
+        if (Math.randomBoolean()) {
+            adult.showImage(0)
+        } else {
+            adult2.showImage(0)
+        }
+    }
+    if (health > 6) {
+        if (Math.randomBoolean()) {
+            adult.showImage(0)
+        } else {
+            adult2.showImage(0)
+        }
     }
 })
-loops.everyInterval(60000, function () {
+loops.everyInterval(3600000, function () {
     health += -1
 })
